a1d20e53998cbb72220dbf3ea02762f5a8b84580
josip-paulik <josip.paulik@outlook.com>
2016-12-07 11:51:40 +0100

    Add repository inferace and TodoItem class.

---
+++ TodoSQLRepository/TodoItem.cs
@@ -0,0 +1,53 @@
+﻿using GenericList;
+using System;
+
+namespace Models
+{
+    /// <summary>
+    /// This class represents some kind of activity that must be done.
+    /// </summary>
+    public class TodoItem
+    { 
+        public readonly Guid Id;
+        public string Text { get; set; }
+        public bool IsCompleted { get; set; }
+        
+        /// <summary>
+        /// Nullable date time.
+        /// DateTime is value type and won’t allow nulls. 
+        /// DateTime? is nullable DateTime and will accept nulls. 
+        /// Use null when todo completed date does not exist (e.g. todo is still not completed) 
+        /// </summary>
+        public DateTime? DateCompleted { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        /// <summary>
+        /// UserId that owns this TodoItem.
+        /// </summary>
+        public Guid UserId { get; set; }
+
+        public TodoItem(string text, Guid userId)
+        {
+            Id = new Guid();
+            Text = text;
+            IsCompleted = false;
+            DateCreated = DateTime.Now;
+            UserId = userId;
+        }
+
+        public TodoItem()
+        {
+            
+        }
+
+        public void MarkAsCompleted()
+        {
+            if(!IsCompleted)
+            {
+                IsCompleted = true;
+                DateCompleted = DateTime.Now;
+            }
+        }
+    }
+}
